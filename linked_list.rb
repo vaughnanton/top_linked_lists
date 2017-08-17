ac2fce3b273f19c445c3fc15bc9ab623f166d817
Vaughn Antonyan <vaughnanton@gmail.com>
2017-08-17 14:57:49 -0700

    define methods

--- linked_list.rb
+++ linked_list.rb
@@ -1,3 +1,49 @@
-#initialize class file
+class LinkedList
 
-LinkedList
+  def append
+  end
+
+  def prepend
+  end
+
+  def size
+  end
+
+  def head
+  end
+
+  def tail
+  end
+
+  def at(index)
+  end
+
+  def pop
+  end
+
+  def contains?
+  end
+
+  def find(data)
+  end
+
+  def to_s
+  end
+
+  def insert_at(index)
+  end
+
+  def remove_at(index)
+  end
+
+end
+
+class Node
+
+  def value
+  end
+
+  def next_node
+  end
+
+end
